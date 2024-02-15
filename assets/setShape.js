0992403369b4f2e6d23cd9875e1c88ca95e1e3cb
Samira Chetta <chettasamira@gmail.com>
2024-02-14 19:23:58 -0500

    added js to make user's desired shape

--- assets/setShape.js
+++ assets/setShape.js
@@ -1,1 +1,26 @@
 
+//match user response to shape class to make new shape
+
+const Circle = require('./Circle.js');
+const Square = require('./Square.js');
+const Triangle = require('./Triangle.js');
+
+function setShape(response) {
+
+    if (response.shape === 'Circle') {
+        let userShape = new Circle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Square') {
+        let userShape = new Square (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+
+    if (response.shape === 'Triangle') {
+        let userShape = new Triangle (response.shapeColor, response.text, response.textColor)
+        return userShape.render()
+    }
+};
+
+module.exports = setShape;
