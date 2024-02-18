283bbd851c1760beb25ae1370b6b5318d536f94c
Samira Chetta <chettasamira@gmail.com>
2024-02-17 20:14:56 -0500

    performed tests to generate sample svgs

---
+++ tests/triangle.test.js
@@ -0,0 +1,16 @@
+const Triangle = require('../Triangle.js');
+
+describe('Triangle', () => {
+    it('should return logo with user choice of shape color, tex, and text color', () => {
+        const triangle = new Triangle('aqua','SYC','lightpink');
+        expect(triangle.render()).toEqual(`
+        <svg version="1.1" 
+            width="300" height="200"
+            xmlns="http://www.w3.org/2000/svg">
+            <polygon points="100, 15 200, 200 0, 200" fill="aqua"/>
+            <text x="100" y="185" font-size="70" text-anchor="middle" fill="lightpink">SYC</text>
+        </svg>
+        `
+        );
+    });
+});
